755d176da9ea0590c339c136684c36a75fc3db2c
thathsara <wthathsaradinuwan@gmail.com>
2026-07-18 16:56:56 +0530

    change directories

--- operion-client/src/routes/index.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-import { useAuthStore } from "@/stores/auth-store";
-
-export const Route = createFileRoute("/")({
-  beforeLoad: () => {
-    const token = useAuthStore.getState().token;
-    throw redirect({ to: token ? "/dashboard" : "/login" });
-  },
-});
